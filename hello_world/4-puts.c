d13cee26c0483e38f126719971caee84599d0833
jessasesh <jessica.dison@yahoo.com>
2023-05-25 11:16:52 -0700

    Q4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #!/bin/bash
 #include <studio.h>
 
-int main(void) {
+int main(void) 
+{
 
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
