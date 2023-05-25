38c756783a08cf4f01b60cc178f76674c0a85b05
jessasesh <jessica.dison@yahoo.com>
2023-05-25 11:57:41 -0700

    Q5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main- Prints "with proper grammar, but the outcome is a piece of art,", followed by a new line
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
