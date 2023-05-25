dd9fb7b20481b1526c70e3358e73d347d6b48671
jessasesh <jessica.dison@yahoo.com>
2023-05-25 12:07:32 -0700

    Q5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
 /**
- * main- Prints "with proper grammar, but the outcome is a piece of art,", followed by a new line
+ * main- Prints with proper grammar,
+ * but the outcome is a piece of art,
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
