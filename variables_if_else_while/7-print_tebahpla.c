cb53396744c67afcc2d7314ede7ccc96a31a9e9c
jessasesh <jessica.dison@yahoo.com>
2023-05-30 06:00:26 -0700

    Q7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char j;
+
+	for (j = 'z'; j >= 'a'; j++)
+	{
+		putchar(j);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
