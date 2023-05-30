55c7e0debbf3c2449a29ff0ab7f14f432ea43b18
jessasesh <jessica.dison@yahoo.com>
2023-05-30 05:57:15 -0700

    Q6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		putchar(j);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
