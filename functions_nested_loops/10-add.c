4107d8584ac9c1c186a4af7ba34938a8c7e2de9f
jessasesh <jessica.dison@yahoo.com>
2023-05-30 18:26:49 -0700

    Q10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - CHECK YOUR PROTOTYPE
+ * @j: CHECK YOUR VAR
+ * @d: CHECK YOUR VAR
+ * Return: CHECK YOUR PROTOTYPE
+ */
+int add(int j, int d)
+{
+	return (j + d);
+}
