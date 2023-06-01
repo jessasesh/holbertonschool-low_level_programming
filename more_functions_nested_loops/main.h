94501e6d64ea80e0dc8fb2993ac6b5de49506a37
jessasesh <jessica.dison@yahoo.com>
2023-05-31 19:21:24 -0700

    file creation

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+/*This file includes all prototypes of all functions
+ * and the prototype of the function _putchar
+ */
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+
+#endif
