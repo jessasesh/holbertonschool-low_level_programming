0fdd0159850524bba4f732372c7ca3e64d7c3934
jessasesh <jessica.dison@yahoo.com>
2023-06-01 09:23:35 -0700

    Q2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * mul - prototype that mutiplies 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of equation
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
