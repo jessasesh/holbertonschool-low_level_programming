a8550ccd1787f28828fb444cfdd9fd190ada300a
jessasesh <jessica.dison@yahoo.com>
2023-06-02 08:09:57 -0700

    Q4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * print_most_numbers - prototype that prints #s from 0-9 except 2&4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if ((j != 2) && (j != 4))
+			putchar(j);
+	}
+	putchar('\n');
+
+}
