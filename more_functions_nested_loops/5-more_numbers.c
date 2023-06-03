974f045a20a1d624f32d34590698720bb7c9468e
jessasesh <jessica.dison@yahoo.com>
2023-06-03 07:01:05 -0700

    Q5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,15 @@
 void more_numbers(void)
 {
 	int j;
+	int d;
 	char c[] = "01234567891011121314";
 
 	for (j = 0; j <= 19; j++)
 	{
+		for (d = 0; d <= 10; d++)
+		{
 		putchar(c[j]);
+		}
 	}
 	putchar('\n');
 }
