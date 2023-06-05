da2b2c8bd9988354cae7c023f685147431e5955b
jessasesh <jessica.dison@yahoo.com>
2023-06-05 16:39:30 -0700

    Q9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * main - prints #s 1-100 with FizzBuzz
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j <= 100; j++)
+	{
+		if (j % 3 == 0 && j % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (j % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (j % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", j);
+		}
+	if (j < 100)
+	printf(" ");
+	}
+printf("\n");
+return (0);
+}
