cefbb3ceff52e024cddb9dbb208279934aacd08e
jessasesh <jessica.dison@yahoo.com>
2023-06-05 16:51:50 -0700

    Q9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int j;
 
-	for (j = 0; j <= 100; j++)
+	for (j = 1; j <= 100; j++)
 	{
-		if (((j % 3) == 0) && ((j % 5) == 0))
+		if (j % 3 == 0 && j % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
