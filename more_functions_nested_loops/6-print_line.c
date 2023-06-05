1b9130552dbff789b5bb9a645b64a0babdc66252
jessasesh <jessica.dison@yahoo.com>
2023-06-05 11:20:47 -0700

    Q6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print_line - prototype that draws a straight line
+ * @n: variable given
+ * Return: void
+ */
+void print_line(int n);
+
+{
+	int j;
+
+	if (n > 0)
+	{
+		for (j = 0; j < n; j++)
+		{
+			putchar('_');
+		}
+		putchar('\n');
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
