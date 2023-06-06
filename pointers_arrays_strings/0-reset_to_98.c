12580c7baf590d5a3997e44f0cd8585108c8da5a
jessasesh <jessica.dison@yahoo.com>
2023-06-05 17:40:06 -0700

    Q0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * reset_to_98 - updates value being pointed at to 98
+ * @n: variable being updated
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
