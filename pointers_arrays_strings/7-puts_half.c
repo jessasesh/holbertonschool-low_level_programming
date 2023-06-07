07bcb8aa81dea2e240e5acc10f6cadc151124e95
jessasesh <jessica.dison@yahoo.com>
2023-06-07 10:01:42 -0700

    Q7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,30 @@
 #include"main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - function
+ * @str: input
+ */
+
+void puts_half(char *str)
+{
+	int x;
+	int length = _strlen(str);
+
+	for (x = 0; x < length; x++)
+	{
+		if (length % 2 != 0 && x < ((length / 2) + 1))
+		{
+			continue;
+		}
+		else if (length % 2 == 0 && x < (length / 2))
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(str[x]);
+		}
+	}
+	_putchar('\n');
+}
