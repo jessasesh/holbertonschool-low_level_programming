4dc8a7a9243a5c1b40aad3b0f415f860236cc4ca
jessasesh <jessica.dison@yahoo.com>
2023-12-11 06:32:23 -0800

    Fixed betty

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,39 +1,33 @@
+#include "lists.h"
+
 /**
  * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
  * @head: Pointer to the head of the dlistint_t list.
  * @n: Integer for the new node to contain.
  *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new node.
+ * Return: Address of new node/ Null upon failure.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    /* Declare a pointer to a new dlistint_t node */
-    dlistint_t *new;
+	/* Declare and allocate memory for a new node */
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
-    /* Allocate memory for the new node */
-    new = malloc(sizeof(dlistint_t));
-    
-    /* Check if memory allocation was successful */
-    if (new == NULL)
-        return (NULL);
+	/* Check if memory allocation was successful */
+	if (new == NULL)
+		return (NULL);
 
-    /* Set the data of the new node */
-    new->n = n;
+	/* Initialize the new node */
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
 
-    /* Set the 'prev' pointer of the new node to NULL */
-    new->prev = NULL;
+	/* Update the 'prev' pointer of the current head if it exists */
+	if (*head != NULL)
+		(*head)->prev = new;
 
-    /* Set the 'next' pointer of the new node to the current head */
-    new->next = *head;
+	/* Update the head pointer to the new node */
+	*head = new;
 
-    /* If the current head is not NULL, update the 'prev' pointer of the current head */
-    if (*head != NULL)
-    (*head)->prev = new;
-
-    /* Update the head pointer to point to the new node */
-    *head = new;
-
-    /* Return the address of the new node */
-    return (new);
+	/* Return the address of the new node */
+	return (new);
 }
