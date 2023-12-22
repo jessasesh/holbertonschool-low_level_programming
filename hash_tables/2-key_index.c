501bbb467d4003d51bb277e28374292d44dfc22c
jessasesh <jessica.dison@yahoo.com>
2023-12-22 04:36:07 -0800

    Fixed betty T2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,20 +1,20 @@
 #include "hash_tables.h"
 
 /**
- * key_index - index where key pair should be stored
- * @key: key to get the index
- * @size: size of array of hash table
+ * key_index - index of key
+ * @key: key to get the index of
+ * @size: size of the arry of the hash table
  *
- * Return: index of the key
+ * Return: index of key
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	/* Compute hash value using djb2*/
-	unsigned long int hash_value = hash_djb2(key);
+	unsigned long int hash_value, index;
+	
+	hash_value = hash_djb2(key);
 
-	/* Calculates index by taking modulo of the hash value and the table size */
-	unsigned long int index = hash_value % size;
+	index = hash_value % size;
 
 	return (index);
 }
