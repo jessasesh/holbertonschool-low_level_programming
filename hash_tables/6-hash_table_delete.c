b56caf652c27c01592b842f8aff7e92e7218c8b9
jessasesh <jessica.dison@yahoo.com>
2023-12-22 05:41:08 -0800

    Fixed betty & changed perm T6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+/**
+ * hash_table_delete - deletes hash table
+ * @ht: pointer to hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *cnode, *nnode;
+	unsigned long int index;
+
+	for (index = 0; index < ht->size; index++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			cnode = ht->array[index];
+			while (cnode)
+			{
+				nnode = cnode->next;
+				free(cnode->key);
+				free(cnode->value);
+				free(cnode);
+				cnode = nnode;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
