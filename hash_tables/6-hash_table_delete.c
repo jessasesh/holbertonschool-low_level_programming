417163161647d5d3d71943c98c43475d8a675f29
jessasesh <jessica.dison@yahoo.com>
2023-12-22 05:45:23 -0800

    Fixed betty T6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -12,7 +12,7 @@ void hash_table_delete(hash_table_t *ht)
 
 	for (index = 0; index < ht->size; index++)
 	{
-		if (ht->array[i] != NULL)
+		if (ht->array[index] != NULL)
 		{
 			cnode = ht->array[index];
 			while (cnode)
